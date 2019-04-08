a4d200bddcfab94e983e50009ac4a95c137ea60e
Administrator <Administrator@windows10.microdone.cn>
2019-04-08 09:23:27 +0800

    zzz

--- src/main/java/com/zzz/springboot/entity/Mail.java
+++ src/main/java/com/zzz/springboot/entity/Mail.java
@@ -9,8 +9,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Mail {
